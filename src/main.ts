46ee6a4e71996ff28d6a5e1ab1f0a503b20e1be1
Matheus Calegaro <hello@matheus.me>
2022-01-19 01:18:41 -0300

    chore(interceptors): add class-transformer's object conversion global interceptor

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 
 import { AppModule } from './app.module';
+import { TransformInterceptor } from './transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -11,6 +12,7 @@ async function bootstrap() {
   );
 
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new TransformInterceptor());
 
   await app.listen(process.env.PORT || 8080, process.env.HOST || '0.0.0.0');
 }
