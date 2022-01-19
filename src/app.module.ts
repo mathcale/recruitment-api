a44c80b68a616efbea542ddd8b4f2c6cc247261e
Matheus Calegaro <hello@matheus.me>
2022-01-19 00:01:03 -0300

    feat(app): configures ConfigModule and TypeOrmModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,39 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DATABASE_HOST'),
+        port: +configService.get<number>('DATABASE_PORT'),
+        username: configService.get('DATABASE_USER'),
+        password: configService.get('DATABASE_PASSWORD'),
+        database: configService.get('DATABASE_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        autoLoadEntities: true,
+        synchronize: process.env.NODE_ENV !== 'production',
+        migrations: [__dirname + '/migrations/*.migration{.ts,.js}'],
+        cli: {
+          migrationsDir: __dirname + '/migrations',
+        },
+        logging:
+          process.env.NODE_ENV !== 'production'
+            ? 'all'
+            : ['error', 'schema', 'warn', 'info', 'log', 'migration'],
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
