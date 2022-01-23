a94bf52b1bbab5db830fea7928b38c1b5149f818
Matheus Calegaro <hello@matheus.me>
2022-01-23 02:39:56 -0300

    chore(main): configure fastify-cookie, fastify-csrf and fastify-helmet modules

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import { fastifyHelmet } from 'fastify-helmet';
+import fastifyCookie from 'fastify-cookie';
+import fastifyCsrf from 'fastify-csrf';
 
 import { AppModule } from './app.module';
 import { TransformInterceptor } from './transform.interceptor';
@@ -13,6 +16,10 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
+
+  await app.register(fastifyHelmet);
+  app.register(fastifyCookie, { secret: process.env.COOKIE_SECRET });
+  await app.register(fastifyCsrf, { cookieOpts: { signed: true } });
 
   await app.listen(process.env.PORT || 8080, process.env.HOST || '0.0.0.0');
 }
